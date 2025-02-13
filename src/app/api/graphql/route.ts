a59f8a4219072c6101094a721d2bbc66012c8730
gm <gilles.mastropasqua@gmail.com>
2025-02-13 12:14:28 +0100

    Add graphql api

--- src/app/api/graphql/route.ts
+++ src/app/api/graphql/route.ts
@@ -1,10 +1,19 @@
 import { createYoga } from "graphql-yoga";
 import { schema } from "@/graphql/schema";
 
-export const { handleRequest } = createYoga({
-    schema,
-    graphqlEndpoint: "/api/graphql",
-    fetchAPI: { Response },
-});
+// Next.js requires a Response object to be available in the global scope
+interface NextContext {
+    params: Promise<Record<string, string>>
+}
 
-export { handleRequest as GET, handleRequest as POST };
+const { handleRequest } = createYoga<NextContext>({
+    schema: schema,
+
+    // While using Next.js file convention for routing, we need to configure Yoga to use the correct endpoint
+    graphqlEndpoint: '/api/graphql',
+
+    // Yoga needs to know how to create a valid Next response
+    fetchAPI: { Response }
+})
+
+export { handleRequest as GET, handleRequest as POST, handleRequest as OPTIONS }
