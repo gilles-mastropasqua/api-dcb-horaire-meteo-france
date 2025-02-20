0471523ebaa442bdffe0f2cd799e9302b854df5c
gm <gilles.mastropasqua@gmail.com>
2025-02-20 18:56:38 +0100

    Create graphql api with Pothos and Yoga

--- src/graphql/builder.ts
+++ src/graphql/builder.ts
@@ -2,34 +2,8 @@ import SchemaBuilder from '@pothos/core';
 import PrismaPlugin from '@pothos/plugin-prisma';
 import { PrismaClient } from '@prisma/client';
 import PrismaTypes from '@pothos/plugin-prisma/generated';
-// import { GraphQLScalarType } from 'graphql/type';
-// import { Kind } from 'graphql/language';
-
 
 const prisma = new PrismaClient();
-
-// const DateTimeScalar = new GraphQLScalarType({
-//     name: 'DateTime',
-//     description: 'ISO 8601 formatted date string',
-//     serialize: (value: unknown) => {
-//         if (value instanceof Date) {
-//             return value.toISOString(); // Convertit Date en string
-//         }
-//         throw new Error('DateTime must be a valid Date object');
-//     },
-//     parseValue: (value: unknown) => {
-//         if (typeof value === 'string') {
-//             return new Date(value); // Convertit string en Date
-//         }
-//         throw new Error('DateTime must be an ISO 8601 string');
-//     },
-//     parseLiteral: (ast) => {
-//         if (ast.kind === Kind.STRING) {
-//             return new Date(ast.value);
-//         }
-//         return null;
-//     },
-// });
 
 export const builder = new SchemaBuilder<{
     PrismaTypes: PrismaTypes;
